1e5d0d2d269ca1a07498de0e887f452eb69d491a
Sebastian- <hmurgu@interchange.ubc.ca>
2018-06-03 17:36:42 -0700

    Added autocomplete functionality to search input

--- js/viewmodels/parkMapViewModel.js
+++ js/viewmodels/parkMapViewModel.js
@@ -1,35 +1,49 @@
 var parkMapViewModel = function() {
     var self = this;
 
-    self.map;
+    self.googleMap;
     self.parks = ko.observableArray();
     self.isParkListVisible = ko.observable(true);
+    self.searchMap = new Map();
     /* TODO map park id to markers to manage showing and hiding markers
     self.idToMarker = Map
     */
 
-    // Load park data
     $.ajax({
         type: "GET",
         url: "static/parks_facilities.xml",
         dataType: "xml",
-        success: function(xml) {
-            // Initialize list of parks in sidebar
-            var $parks = $(xml).find("Park");
+        success: function(xmlResponse) {
+            // Initialize parks observable array
+            var $parks = $(xmlResponse).find("Park");
             $parks.each(function(index, xmlPark) {
-                self.parks.push(new Park(xmlPark));
+                var park = new Park(xmlPark);
+                self.parks.push(park);
             });
             // A filler <li> for easier navigation of the last elements in list
             $("#parkList").append("<li class='lastListItem'></li>");
+        },
+        complete: function() {
+            // Initialize searchMap
+            self.parks().forEach(function(park) {
+                self.addToSearchMap(park.name, park.id);
+                self.addToSearchMap(park.neighbourhood, park.id);
+                park.facilities.forEach(function(facility) {
+                    self.addToSearchMap(facility.type, park.id);
+                });
+            });
+            self.initSearchBox();
         }
     });
 
-    self.toggleParkList = function() {
-        self.isParkListVisible(!self.isParkListVisible());
-    }
+    self.initSearchBox = function() {
+        $("#searchBarInput").autocomplete({
+            source: Array.from(self.searchMap.keys())
+        });
+    };
 
-    self.initMap = function() {
-        self.map = new google.maps.Map(document.getElementById('map'), {
+    self.initGoogleMap = function() {
+        self.googleMap = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 49.255, lng: -123.130},
             zoom: 13,
             mapTypeControl: false
@@ -44,10 +58,23 @@ var parkMapViewModel = function() {
             bounds.extend(latLng);
             var marker = new google.maps.Marker({
                 position: latLng,
-                map: self.map
+                map: self.googleMap
             });
         });
-        self.map.fitBounds(bounds);
+        self.googleMap.fitBounds(bounds);
+    };
+
+    self.toggleParkList = function() {
+        self.isParkListVisible(!self.isParkListVisible());
+    };
+
+    // Utility functions
+    self.addToSearchMap = function(key, parkID) {
+        if(self.searchMap.has(key)) {
+            self.searchMap.get(key).add(parkID);
+        } else {
+            self.searchMap.set(key, new Set(parkID));
+        }
     };
 
 }
